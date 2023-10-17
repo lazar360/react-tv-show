28bca41df67cbc393e5b963c62f6ee34182b389a
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-10-17 20:50:52 +0200

    maj script

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
-export function App(){
-    return <div>Hello World</div>
+import { TVShowAPI } from "./api/tv-show";
+import "./global.css";
+import s from "./style.module.css";
+
+TVShowAPI.fetchPopulars();
+
+export function App() {
+  return (
+    <div className={s.main_container}>
+      <div className={s.header}>
+        <div className="row">
+            <div className="col-4">
+                <div>Logo</div>
+                <div>Subtitle</div>
+            </div>
+            <div className="col-sm-12 col-md-4">
+                <input type="text" />
+            </div>
+        </div>
+      </div>
+      <div className={s.tv_show_detail}>Detail</div>
+      <div className={s.recommentations}>Recommandations</div>
+    </div>
+  );
 }
