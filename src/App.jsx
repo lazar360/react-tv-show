5f994398d1e7d7ba7a405c690d845d64c43d19c4
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-10-31 17:03:42 +0100

    maj script

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
+import { useState } from "react";
 import { TVShowAPI } from "./api/tv-show";
 import "./global.css";
 import s from "./style.module.css";
+import { useEffect } from "react";
 
 TVShowAPI.fetchPopulars();
 
 export function App() {
+  const [currentTvShow, setCurrentTVShow] = useState();
+
+  async function fetchPopulars() {
+    const populars = await TVShowAPI.fetchPopulars();
+    if (populars.length > 0) {
+      setCurrentTVShow(populars[0]);
+    }
+  }
+
+  useEffect(() => {
+    fetchPopulars();
+  }, []);
+  console.log("***", currentTvShow);
   return (
     <div className={s.main_container}>
       <div className={s.header}>
         <div className="row">
-            <div className="col-4">
-                <div>Logo</div>
-                <div>Subtitle</div>
-            </div>
-            <div className="col-sm-12 col-md-4">
-                <input type="text" />
-            </div>
+          <div className="col-4">
+            <div>Logo</div>
+            <div>Subtitle</div>
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <input type="text" />
+          </div>
         </div>
       </div>
       <div className={s.tv_show_detail}>Detail</div>
