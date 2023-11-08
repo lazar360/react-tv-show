98ba0425711280741f9f482c281bf6ee82f95f3e
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-11-08 22:03:18 +0100

    maj script

--- src/components/TVShowDetails/TVShowDetails.jsx
+++ src/components/TVShowDetails/TVShowDetails.jsx
@@ -1,4 +1,10 @@
 import s from "./style.module.css";
-export function TVShowDetail(props){
-    return <div></div>;
+export function TVShowDetail({ tvShow }) {
+  return (
+    <div>
+      <div className={s.title}>{tvShow.name}</div>
+      <div  className={s.rating}>{tvShow.vote_average/2}</div>
+      <div className={s.overview}>{tvShow.overview}</div>
+    </div>
+  );
 }
