91ace5bec6c9b28f1b7c6e846dda733e1f1ec98d
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-11-20 18:46:32 +0100

    maj script

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -4,7 +4,11 @@ import { API_KEY_PARAM, BASE_URL } from "../config";
 export class TVShowAPI {
     static async fetchPopulars(){
         const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
-        console.log("response.data.results", response.data.results);
+        return response.data.results;
+    }
+
+    static async fetchRecommendations(TVShowId){
+        const response = await axios.get(`${BASE_URL}tv/${TVShowId}/recommendations${API_KEY_PARAM}`);
         return response.data.results;
     }
 }
