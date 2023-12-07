5cc076d0d8530861694b8cd4d200f8638782268f
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-12-07 13:56:01 +0100

    maj script

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -2,13 +2,32 @@ import axios from "axios";
 import { API_KEY_PARAM, BASE_URL } from "../config";
 
 export class TVShowAPI {
+    
     static async fetchPopulars(){
-        const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
-        return response.data.results;
+        try {
+            const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
+            return response.data.results;
+        } catch (error) {
+            alert("Erreur durant la recherche des séries populaires");
+        }
     }
 
     static async fetchRecommendations(TVShowId){
-        const response = await axios.get(`${BASE_URL}tv/${TVShowId}/recommendations${API_KEY_PARAM}`);
-        return response.data.results;
+        try {
+            const response = await axios.get(`${BASE_URL}tv/${TVShowId}/recommendations${API_KEY_PARAM}`);
+            return response.data.results;
+        } catch (error) {
+            alert("Erreur durant la recherche des séries populaires");
+        }
     }
+
+    static async fetchByTitle(title){
+        try {
+            const response = await axios.get(`${BASE_URL}search/tv${API_KEY_PARAM}&query=${title}`);
+            return response.data.results;
+        } catch (error) {
+            alert("Erreur durant la recherche des séries populaires");
+        }
+    }
+
 }
