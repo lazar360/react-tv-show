8e5f50fc052ae07466bafbdd17288b5bf2ceaaf7
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-12-11 21:11:31 +0100

    maj script

--- src/components/TVShowListItem/TVShowListItem.jsx
+++ src/components/TVShowListItem/TVShowListItem.jsx
@@ -1,14 +1,15 @@
 import { SMALL_IMG_COVER_BASE_URL } from "../../config";
 import s from "./style.module.css";
-import noimage from "../../assets/images/no-image.png";
+import noimage from "../../assets/images/no-image.png"; 
 
 export function TVShowListItem({ tvShow, onClick }) {
+
   return (
     <div onClick={() => onClick(tvShow)} className={s.container}>
       <img
         alt={tvShow.name}
         className={s.img}
-        src={SMALL_IMG_COVER_BASE_URL + tvShow.backdrop_path}
+        src={SMALL_IMG_COVER_BASE_URL + tvShow.backdrop_path || noimage}
       />
       <div className={s.title}>{tvShow.name}</div>
     </div>
