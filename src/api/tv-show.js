83d978e539c3645fcba47963297a48708c11f0b2
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-12-18 20:51:34 +0100

    maj script

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -2,32 +2,34 @@ import axios from "axios";
 import { API_KEY_PARAM, BASE_URL } from "../config";
 
 export class TVShowAPI {
-    
-    static async fetchPopulars(){
-        try {
-            const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
-            return response.data.results;
-        } catch (error) {
-            alert("Erreur durant la recherche des séries populaires");
-        }
+  static async fetchPopulars() {
+    try {
+      const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
+      return response.data.results;
+    } catch (error) {
+      alert("Erreur durant la recherche des séries populaires");
     }
+  }
 
-    static async fetchRecommendations(TVShowId){
-        try {
-            const response = await axios.get(`${BASE_URL}tv/${TVShowId}/recommendations${API_KEY_PARAM}`);
-            return response.data.results;
-        } catch (error) {
-            alert("Erreur durant la recherche des séries populaires");
-        }
+  static async fetchRecommendations(TVShowId) {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}tv/${TVShowId}/recommendations${API_KEY_PARAM}`
+      );
+      return response.data.results;
+    } catch (error) {
+      alert("Erreur durant la recherche des séries populaires" + error.message);
     }
+  }
 
-    static async fetchByTitle(title){
-        try {
-            const response = await axios.get(`${BASE_URL}search/tv${API_KEY_PARAM}&query=${title}`);
-            return response.data.results;
-        } catch (error) {
-            alert("Erreur durant la recherche des séries populaires");
-        }
+  static async fetchByTitle(title) {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}search/tv${API_KEY_PARAM}&query=${title}`
+      );
+      return response.data.results;
+    } catch (error) {
+      alert("Erreur durant la recherche des séries populaires");
     }
-
+  }
 }
